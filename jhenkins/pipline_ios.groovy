be23cfe584afa0811e14a21ed15c359764d906ce
mohamedmortdha <mohamedmortdhaguedhami@hotmail.fr>
2021-01-24 20:32:53 +0100

    jhenkins pipline

--- jhenkins/pipline_ios.groovy
+++ jhenkins/pipline_ios.groovy
@@ -1,12 +1,52 @@
+#!/usr/bin/env groovy
+def DEVICE
 pipeline {
     agent any
-
+    parameters {
+            string(defaultValue: 'iPhone XS', description: 'Device name', name: 'DevicesName', trim: true)
+            string(defaultValue: 'bs://167020785b0914f222af4be7b8f8181fde5f0f26', description: 'URL Application', name: 'AppUrl', trim: true)
+            string(defaultValue: '14.0', description: 'Operating System version', name: 'os_version', trim: true)
+            string(defaultValue: 'Ios_en_callback_brand', description: 'Test Execution', name: 'tags', trim: true)
+    }
     stages {
-        stage('Hello') {
+        stage('send notification') {
             steps {
-                sh 'python -m pip install -r requirements.txt'
-                sh 'robot -i  Ios_en_callback_brand_off_ligne  .'
+                mail to: "${params.notifie}",
+                subject: "Test Automation Mobile Pipline ${env.BUILD_TAG} Test Execution ${params.TestExecutionKey}",
+                body: "start build ${env.BUILD_TAG} at ${new Date()}"
+            }
+        }
+        stage('Display Docker environment') {
+            steps {
+                sh 'pip3 install -r requirements.txt'
+                script {
+                    DEVICE = params.DevicesName.replaceAll(' ', '_')
+                }
+            }
+        }
+        stage('Connect to browserstack') {
+            steps {
+                browserstack('b41aa05d-5e33-4582-94be-d175eddaaaa2') {
+                }
+            }
+        }
+        stage('Execute tests with robot') {
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                    sh "robot  -v BUILD:${env.BUILD_TAG} -v DEVICE:${DEVICE} -v OS_VERSION:${params.os_version} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i tags ."
+                }
+            }
+        }
+        stage('Publish Robot Framework HTML report') {
+            steps {
+                robot outputPath: '', passThreshold: 100.0, unstableThreshold: 100.0
+            }
+        }
+        stage('Attache BrowserStack test results') {
+            steps {
+                browserStackReportPublisher 'app-automate'
             }
         }
     }
 }
+
