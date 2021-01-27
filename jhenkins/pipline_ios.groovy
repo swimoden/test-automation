49fdac31a21b7e7b74f5e92d8d3c08767f1fe457
mohamedmortdha <mohamedmortdhaguedhami@hotmail.fr>
2021-01-27 21:57:35 +0100

    add second phone test

--- jhenkins/pipline_ios.groovy
+++ jhenkins/pipline_ios.groovy
@@ -1,13 +1,15 @@
 #!/usr/bin/env groovy
 def DEVICE
+def DEVICE2
 pipeline {
     agent any
     parameters {
-            string(defaultValue: 'iPhone XS', description: 'Device name', name: 'DevicesName', trim: true)
-            string(defaultValue: 'iPhone XS', description: 'Device name', name: 'SecondDevicesName', trim: true)
             string(defaultValue: 'bs://167020785b0914f222af4be7b8f8181fde5f0f26', description: 'URL Application', name: 'AppUrl', trim: true)
-            string(defaultValue: '14.0', description: 'Operating System version', name: 'os_version', trim: true)
-            string(defaultValue: 'Ios_en_callback_brand', description: 'Test Execution', name: 'tags', trim: true)
+            string(defaultValue: 'iPhone XS', description: 'Device name', name: 'DevicesName', trim: true)
+            string(defaultValue: '14.0', description: 'Operating System version phone1', name: 'os_version_phone1', trim: true)
+            string(defaultValue: 'iPhone XS', description: 'Device name', name: 'SecondDevicesName', trim: true)
+            string(defaultValue: '14.0', description: 'Operating System version phone2', name: 'os_version_phone2', trim: true)
+            string(defaultValue: 'Ios_en_callback_brand', description: 'Test tags', name: 'tags', trim: true)
     }
     stages {
         stage('pre requise install') {
@@ -16,6 +18,7 @@ pipeline {
                 sh 'python3.8 -m pip freeze'
                 script {
                     DEVICE = params.DevicesName.replaceAll(' ', '_')
+                    DEVICE2 = params.SecondDevicesName.replaceAll(' ', '_')
                 }
             }
         }
@@ -28,14 +31,14 @@ pipeline {
         stage('Execute tests with robot with first phone') {
             steps {
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                    sh "python3.8 -m robot  -v BUILD:${env.BUILD_TAG} -v DEVICE:${DEVICE} -v OS_VERSION:${params.os_version} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags} ."
+                    sh "python3.8 -m robot  -v BUILD:${env.BUILD_TAG} -v DEVICE:${DEVICE} -v OS_VERSION:${params.os_version_phone1} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags} ."
                 }
             }
         }
         stage('Execute tests with robot with second phone') {
             steps {
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                    sh "python3.8 -m robot  -v BUILD:${env.BUILD_TAG} -v DEVICE:${SecondDevicesName} -v OS_VERSION:${params.os_version} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags} ."
+                    sh "python3.8 -m robot  -v BUILD:${env.BUILD_TAG} -v DEVICE:${SecondDevicesName} -v OS_VERSION:${params.os_version_phone2} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags} ."
                 }
             }
         }
