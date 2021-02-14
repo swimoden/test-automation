7f6745e24111c15aee5c9004cdcab2440b459c76
mohamedmortdha <mohamedmortdhaguedhami@hotmail.fr>
2021-02-14 21:56:44 +0100

    ios pipline

--- jhenkins/pipline_ios.groovy
+++ jhenkins/pipline_ios.groovy
@@ -1,11 +1,50 @@
+#!/usr/bin/env groovy
+def DEVICE
 pipeline {
     agent any
-
+    parameters {
+            string(defaultValue: 'Samsung Galaxy A51', description: 'Device name', name: 'DevicesName', trim: true)
+            string(defaultValue: 'bs://5a655452000a8ab33b25e14a3ed2c84f403846c7', description: 'URL Application', name: 'AppUrl', trim: true)
+            string(defaultValue: '14.0', description: 'Operating System version', name: 'os_version', trim: true)
+            string(defaultValue: 'Phone 11 Pro Max', description: 'Test Execution', name: 'tags', trim: true)
+    }
     stages {
-        stage('Hello') {
+        stage('pre requise install') {
             steps {
-                sh 'python -m pip install -r requirements.txt'
-                sh 'robot -i  Ios_en_callback_brand_off_ligne  .'
+                sh 'python3.8 -m pip install -r requirements.txt'
+                sh 'python3.8 -m pip freeze'
+                script {
+                    DEVICE = params.DevicesName.replaceAll(' ', '_')
+                }
+            }
+        }
+        stage('Connect to browserstack') {
+            steps {
+                browserstack('3aac3daa-8942-481b-9716-c75251d9aa7c') {
+                }
+            }
+        }
+        stage('Execute tests with robot with ') {
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                    sh "python3.8 -m robot  --outputdir report  -v BUILD:${env.BUILD_TAG} -v DEVICE:${DEVICE} -v OS_VERSION:${params.os_version} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags}  ."
+                }
+            }
+        }
+
+        stage('Publish Robot Framework HTML report') {
+            steps {
+                robot outputPath: 'report', passThreshold: 100.0, unstableThreshold: 100.0
+            }
+        }
+        stage('Attache BrowserStack test results') {
+            steps {
+                browserStackReportPublisher 'app-automate'
+            }
+        }
+        stage('Publish report Robot Framework') {
+            steps {
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: '', reportFiles: 'report\\report.html', reportName: 'HTML Report', reportTitles: ''])
             }
         }
     }
