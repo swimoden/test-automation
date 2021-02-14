d6002f45c28153b5cd0ac0a1a1f98b187518b2bb
mohamedmortdha <mohamedmortdhaguedhami@hotmail.fr>
2021-02-14 22:07:15 +0100

    pipline ios

--- jhenkins/pipline_ios.groovy
+++ jhenkins/pipline_ios.groovy
@@ -4,10 +4,9 @@ pipeline {
     agent any
     parameters {
             string(defaultValue: 'Samsung Galaxy A51', description: 'Device name', name: 'DevicesName', trim: true)
-            string(defaultValue: 'Samsung Galaxy A51', description: 'Device name', name: 'SecondDevicesName', trim: true)
-            string(defaultValue: 'bs://860a96962ee7510e9068dd1a936ed656a5019233', description: 'URL Application', name: 'AppUrl', trim: true)
-            string(defaultValue: '10.0', description: 'Operating System version', name: 'os_version', trim: true)
-            string(defaultValue: 'Android_en_callback_brand', description: 'Test Execution', name: 'tags', trim: true)
+            string(defaultValue: 'bs://5a655452000a8ab33b25e14a3ed2c84f403846c7', description: 'URL Application', name: 'AppUrl', trim: true)
+            string(defaultValue: '14.0', description: 'Operating System version', name: 'os_version', trim: true)
+            string(defaultValue: 'iPhone 11 Pro Max', description: 'Test Execution', name: 'tags', trim: true)
     }
     stages {
         stage('pre requise install') {
@@ -25,7 +24,7 @@ pipeline {
                 }
             }
         }
-        stage('Execute tests with robot with first phone') {
+        stage('Execute tests with robot') {
             steps {
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
                     sh "python3.8 -m robot  --outputdir report  -v BUILD:${env.BUILD_TAG} -v DEVICE:${DEVICE} -v OS_VERSION:${params.os_version} -v REMOTE_BUILD:True  -v APP:${params.AppUrl}  -i ${params.tags}  ."
